fa1c9a2cf211ed2bcaa9a2269ef271367d727329
Akshaylal <akshaylals2000@gmail.com>
2022-04-05 06:46:12 +0530

    added git

--- setup-vagrant.sh
+++ setup-vagrant.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 
-cd /tmp
+sudo apt-get update && sudo apt-get install -y git
 
-curl https://download.virtualbox.org/virtualbox/6.1.32/virtualbox-6.1_6.1.32-149290~Ubuntu~xenial_amd64.deb --output virtualbox.deb
-dpkg -i virtualbox.deb
+curl https://download.virtualbox.org/virtualbox/6.1.32/virtualbox-6.1_6.1.32-149290~Ubuntu~xenial_amd64.deb --output /tmp/virtualbox.deb
+dpkg -i /tmp/virtualbox.deb
 
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
