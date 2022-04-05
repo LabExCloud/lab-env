0349b8bb45cc3bfe1d592444c494c68821525a11
Akshaylal <akshaylals2000@gmail.com>
2022-04-05 07:47:17 +0530

    addded vscode

--- setup.sh
+++ setup.sh
@@ -15,9 +15,15 @@ sudo apt-get update && sudo apt-get install -y git curl
 
 sudo apt-get -f -y install
 
+# virtualbox
 curl https://download.virtualbox.org/virtualbox/6.1.32/virtualbox-6.1_6.1.32-149290~Ubuntu~xenial_amd64.deb --output /tmp/virtualbox.deb
 sudo dpkg -i /tmp/virtualbox.deb
 
+# vscode
+curl "https://az764295.vo.msecnd.net/stable/e18005f0f1b33c29e81d732535d8c0e47cafb0b5/code_1.66.0-1648620611_amd64.deb" --output /tmp/vscode.deb
+sudo dpkg -i /tmp/vscode.deb
+
+# vagrant
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get update && sudo apt-get install -y vagrant
