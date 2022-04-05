917ac59ea4aa3dc24255913456074ac836157756
Akshaylal <akshaylals2000@gmail.com>
2022-04-05 06:50:39 +0530

    added -y

--- setup-vagrant.sh
+++ setup-vagrant.sh
@@ -7,4 +7,4 @@ dpkg -i /tmp/virtualbox.deb
 
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt-get update && sudo apt-get install vagrant
+sudo apt-get update && sudo apt-get install -y vagrant
